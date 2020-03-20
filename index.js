41ca2bf177110570eb577983cb5d889b8fdef0b2
apex2060 <apex2060@gmail.com>
2020-03-20 13:25:43 -0600

    done. short code redirect now linked

--- index.js
+++ index.js
@@ -12,15 +12,13 @@ var db = firebase.firestore();
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.get('/', (request, response)=>{
-	response.send(`
-		<h1>Hi</h1>
-	`)
+	response.redirect('https://a.alphabetize.us/#/project/resumes');
 })
 app.get('/:id', (request, response)=>{
 	let ref = db.collection(`shortCode/res2me/list`).doc(request.params.id);
 	ref.get().then(r=>{
 		let sc = r.data();
-		response.send(sc)
+		response.redirect(sc.url);
 	})
 })
 
