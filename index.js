6f524b017de7524fdd9873b40a1b911487f88950
apex2060 <apex2060@gmail.com>
2020-03-20 13:18:06 -0600

    link with firebase

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ firebase.initializeApp({
 	databaseURL: process.env.firebase,
 	credential: firebase.credential.cert(JSON.parse(process.env.googleJson))
 });
+var db = firebase.firestore();
+
 
 
 var app = express();
-
 app.set('port', (process.env.PORT || 5000));
-
-
 app.get('/', (request, response)=>{
 	response.send(`
 		<h1>Hi</h1>
 	`)
 })
 app.get('/:id', (request, response)=>{
-	response.send(`
-		<h1>${id}</h1>
-	`)
+	let ref = db.collection(`shortCode/res2me/list`).doc(request.params.id);
+	ref.get().then(r=>{
+		let sc = r;
+		response.send(sc)
+	})
 })
 
 app.listen(app.get('port'), function() {
