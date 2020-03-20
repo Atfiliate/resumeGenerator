2d7ba57586de152cd130b04b2b9b240803498eb0
apex2060 <apex2060@gmail.com>
2020-03-20 13:12:17 -0600

    test route

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-var fire = require("./cloud/fire.js");
-var express = require('express');
+var firebase	= require('firebase-admin');
+var express 	= require('express');
+
+firebase.initializeApp({
+	databaseURL: process.env.firebase,
+	credential: firebase.credential.cert(JSON.parse(process.env.googleJson))
+});
+
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 
+app.get('/', (request, response)=>{
+	response.send(`
+		<h1>Hi</h1>
+	`)
+})
 app.get('/:id', (request, response)=>{
-	
+	response.send(`
+		<h1>${id}</h1>
+	`)
 })
 
 app.listen(app.get('port'), function() {
